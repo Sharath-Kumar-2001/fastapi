719dd3496b5399c91452cce9705aacd0773febae
Sharathkumar V <sharathkumar.v@prodapt.com>
2025-11-17 15:50:20 +0530

    fastapisample

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+  return {"hello": "world"}
+
 @app.get("/health")
 async def health():
   return {"status": "ok"}

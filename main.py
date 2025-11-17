b4db9c3342673de3b2ef52c5f9eb5c11606a68c6
Sharath-Kumar-2001 <sharathkumar.v@prodapt.com>
2025-11-17 16:45:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
-
-@app.get("/")
-async def root():
-  return {"hello": "world deploy"}
 
 @app.get("/health")
 async def health():
   return {"status": "ok"}
+
+class AddResult(BaseModel):
+  result: int
+
+@app.get("/add", response_model=AddResult)
+def add(a:int,b:int):
+  c = a+b
+  return {"result": c}
+
+@app.get("/multiply", response_model=AddResult)
+def multiply(a:int,b:int):
+  c = a*b
+  return {"result": c}
+

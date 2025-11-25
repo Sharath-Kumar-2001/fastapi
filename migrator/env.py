9a99800b94e3fb73b350bd21e24a09f74e0cb96c
Sharathkumar V <sharathkumar.v@prodapt.com>
2025-11-25 14:34:07 +0530

    new db migration applied.

--- migrator/env.py
+++ migrator/env.py
@@ -2,9 +2,13 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from models import Base
+
 from alembic import context
+
 from config import settings
+
+from models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,13 +22,15 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+# target_metadata = None
 target_metadata = Base.metadata
+
 config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
